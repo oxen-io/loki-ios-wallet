dca9f2b5d5bd6734c2d2bbf3f4780a23595920a4
Mikunj <mikunj@live.com.au>
2019-04-01 13:59:09 +1100

    Removed exchange.

--- CakeWallet/Flows/WalletFlow.swift
+++ CakeWallet/Flows/WalletFlow.swift
@@ -33,10 +33,6 @@ final class WalletFlow: NSObject, Flow, UITabBarControllerDelegate {
        return SettingsFlow()
     }()
     
-    private lazy var exchangeFlow: ExchangeFlow = {
-        return ExchangeFlow()
-    }()
-    
     convenience override init() {
         let tabbarController = CustomTabBarController()
         self.init(rootController: tabbarController)
@@ -59,35 +55,8 @@ final class WalletFlow: NSObject, Flow, UITabBarControllerDelegate {
     private func configureRootTabBar() {
         _root.viewControllers = [
             dashboardFlow.rootController,
-//            UINavigationController(rootViewController: ReceivePageViewController(
-//                transitionStyle: .scroll,
-//                navigationOrientation: .horizontal,
-//                options: nil
-//                )
-//            ),
-//            UINavigationController(rootViewController: SendViewController(store: store)),
-            exchangeFlow.rootController,
             settingsFlow.rootController
-//            UINavigationController(rootViewController: SettingsViewController(store: store))
         ]
     }
     
-//    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-//        guard
-//            let nav = viewController as? UINavigationController,
-//            let viewController = nav.viewControllers.first as? DashboardController else { return }
-//
-//        if let nav = viewController.presentedViewController as? UINavigationController {
-//            nav.viewControllers.first?.dismiss(animated: true)
-//        } else {
-//            viewController.presentedViewController?.dismiss(animated: true)
-//            viewController.view.subviews.forEach { view in
-//                    if view.tag == blurViewTag {
-//                        view.removeFromSuperview()
-//                    }
-//            }
-//        }
-//        viewController.navigationItem.titleView?.isHidden = false
-//        viewController.navigationItem.rightBarButtonItem = viewController.syncButton
-//    }
 }
